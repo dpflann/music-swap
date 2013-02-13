020916c749b5587fde08fe15e1fec47811bfb976
Daniel Flannery <daniel.flannery@openstudy.com>
2013-02-13 03:08:29 -0500

    On the stairmaster, one step at time, see Log.txt. Entries have a datamodel, new styling, and can be created for development/testing

--- index.py
+++ index.py
@@ -5,11 +5,31 @@ import cgi
 import os
 import jinja2
 import webapp2
+import datetime
 from models import Entry
 
 template_directory = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_directory), autoescape=True) #autoescaping for escaping html 
 
+##DB METHODS
+def create_entry(original_urls, oembed_urls, oembed_media_urls, body, author, email, date, tags):
+  return Entry(original_urls = original_urls,
+      oembed_urls = oembed_urls,
+      oembed_media_urls = oembed_media_urls,
+      body = body,
+      author = author,
+      email = email,
+      date = date,
+      tags = tags)
+
+def get_entries(update=False):
+  query = db.GqlQuery("SELECT * FROM Entry ORDER BY created DESC")
+  return list(query)
+
+def add_entry(entry):
+  entry.put()
+
+## HANDLERS
 
 class Handler(webapp2.RequestHandler):
   def write(self, *a, **kw):
@@ -25,7 +45,8 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):
   def render_front(self):
-    self.render("index.html")
+    entries = get_entries()
+    self.render("index.html", entries = entries)
 
   def get(self):
     self.render_front()
@@ -33,8 +54,29 @@ class MainPage(Handler):
   def post(self):
     pass
 
+class CreateEntry(Handler):
+  def render_front(self):
+    entry = create_entry(["http://vimeo.com/12622016"],
+        ["http://vimeo.com/12622016"],
+        ["<iframe src=\"http://player.vimeo.com/video/12622016/\" width=\"560\" height=\"400\" frameborder=\"0\" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>"],
+        "The Music Scene by Blockhead, wonderous vivid music video!",
+        "Dan Flan",
+        "danflan@dmail.com",
+        datetime.datetime(2012, 11, 14, 5, 6),
+        ["pirates", "swashbuckling", "turntablism", "Blockhead"])
+    add_entry(entry)
+
+    self.render("creating.html")
+  
+  def get(self):
+    self.render_front()
+
+  def post(self):
+    pass
+
 application = webapp2.WSGIApplication([
-                                    ('/', MainPage)],
+                                    ('/', MainPage),
+                                    ('/create', CreateEntry)],
                                     debug=True)
 def main():
     run_wsgi_app(application)
--- models.py
+++ models.py
@@ -2,12 +2,13 @@ from google.appengine.ext import db
 import webapp2
 
 class Entry(db.Model):
-  def say(self, string):
-    print(string)
-  pass
-
-
-class User(db.Model):
-  pass
-
+  original_urls = db.StringListProperty(required = True)
+  oembed_urls = db.StringListProperty()
+  oembed_media_urls = db.StringListProperty()
+  body = db.TextProperty()
+  author = db.TextProperty()
+  email = db.EmailProperty() #"gordon@example.com"
+  date = db.DateTimeProperty(required = True)
+  created = db.DateTimeProperty(auto_now_add = True)
+  tags = db.StringListProperty()
 
