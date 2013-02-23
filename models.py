d8249090a58dcebaa813190d04b004420039444e
Daniel Flannery <d.p.flannery@gmail.com>
2013-02-23 13:43:55 -0500

    Removed 'tags' field from Entry, update HTML to account for this. Styled entry to a more acceptable format to push project along so that I can get it out sooner.

--- models.py
+++ models.py
@@ -2,13 +2,14 @@ from google.appengine.ext import db
 import webapp2
 
 class Entry(db.Model):
-  original_urls = db.StringListProperty(required = True)
-  oembed_urls = db.StringListProperty()
-  oembed_media_urls = db.StringListProperty()
   body = db.TextProperty()
   author = db.TextProperty()
   email = db.EmailProperty() #"gordon@example.com"
   date = db.DateTimeProperty(required = True)
   created = db.DateTimeProperty(auto_now_add = True)
-  tags = db.StringListProperty()
+  original_urls = db.StringListProperty(required = True)
+  oembed_urls = db.StringListProperty()
+  oembed_media_urls = db.StringListProperty()
+
+
 
